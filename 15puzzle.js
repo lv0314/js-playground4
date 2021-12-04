7d0933eccece030d251afa606bbc09c7d470c6a0
DESKTOP-88VMMCI\user <luzverde314@gmail.com>
2021-12-05 03:21:15 +0900

    no message

---
+++ 15puzzle.js
@@ -0,0 +1,121 @@
+var btnSpace = document.getElementById("btnSpace");
+var getBtn = document.getElementsByClassName("btns");
+var storage = [];
+
+function nums(){
+    for(i = 1; i < 17; i++){
+        storage.push(i);
+    }
+}
+
+function createBtn(){
+    var btn = document.createElement("button");
+    return btn;
+}
+
+function setBtn(btn){
+    btn.setAttribute("class", "btns");
+    btnSpace.appendChild(btn);
+    return btn;
+}
+
+function createBtns(){
+    for (var i = 0; i < 16; i++){
+        var btns = setBtn(createBtn());
+        btns.innerHTML = storage[i];
+        if((i % 4) == 3){
+            btnSpace.innerHTML += "<br>";
+        }   
+    }
+}
+
+function renew(){
+    for(var i = 0; i < 16; i++){
+        getBtn[i].innerHTML = storage[i]
+    }
+}
+
+function swtIdx(a, b){
+    var temp = storage[a];
+    storage[a] = storage[b];
+    storage[b] = temp;
+}
+
+function swtBtn(a, b){
+    if(a-4 == b){swtIdx(a, b); renew();}
+    else if(a-1 == b){swtIdx(a, b); renew();}
+    else if(a+1 == b){swtIdx(a, b); renew();}
+    else if(a+4 == b){swtIdx(a, b); renew();}
+}
+
+function clickNum(){
+    var t = Number(this.innerHTML);
+    var index = storage.indexOf(t);
+    var index16 = storage.indexOf(16);
+    swtBtn(index, index16);
+}
+
+function getClick(){
+    for(i = 0; i < 16; i++){
+        getBtn[i].addEventListener("click", clickNum)
+    }
+}
+
+function setOpbtn(btn){
+    btn.setAttribute("class", "opbtns");
+    opSpace.appendChild(btn);
+    return btn;
+}
+
+function createOpbtns(){
+    var b1 = setOpbtn(createBtn());
+    b1.innerHTML = "SOLVE"
+    b1.setAttribute("id", "solve")
+    b1.addEventListener("click", function(){
+        alertAns(opSolve());
+    })
+
+    var b2 = setOpbtn(createBtn());
+    b2.innerHTML = "SHUFFLE"
+    b2.setAttribute("id", "shuffle")
+    b2.addEventListener("click", function(){
+        opShuffle(storage);
+    })
+}
+
+function opSolve(){
+    var ans = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    for(i = 0; i < 16; i++){
+        if(storage[i] !== ans[i]){
+            return false;
+        } 
+    }
+}
+
+function alertAns(ans){
+    if(typeof(ans) == "undefined"){
+        alert("clear!");
+    } else {
+        alert("try again!");
+    }
+}
+
+function opShuffle(arr){
+    for (var i = 0; i < arr.length; i++){
+        var j = Math.floor(Math.random() * (i+1));
+
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    renew();
+}
+
+function main(){
+    nums();
+    createBtns();
+    createOpbtns();
+    getClick();
+}
+
+main();
